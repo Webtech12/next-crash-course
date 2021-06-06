f649d431495d5cd36277d0b1a6271124ff5fe036
Webtech12 <sherwinlukes@gmail.com>
2021-06-06 18:38:36 +0500

    learning nextjs, these are the basics.

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        <span>WebDev</span> News
+      </h1>
+      <p className={styles.description}>some random text</p>
+    </div>
+  );
+};
+
+export default Header;
